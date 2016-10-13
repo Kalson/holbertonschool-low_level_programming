29f50605c49de20b8debcb3b0f8ba7b8a7850880
Kalson Kalu <Kalson.Kalu@yahoo.com>
2016-10-12 17:30:26 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 /**
- * main - This is my main description
+ * main - prints my message
  */
  int main(void)
  {
   puts("\"Programming is like building a multilingual puzzle");
   return(0);
  }
+
+/**
+ * is_positive - Check if a number is greater than 0
+ * @nb: The number to be checked
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
