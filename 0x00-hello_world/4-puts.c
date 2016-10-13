a28f4f51323026cf30eb90cc5eca010008991400
Kalson Kalu <Kalson.Kalu@yahoo.com>
2016-10-12 17:35:11 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
  * main - prints my message
+ * return - returns the int function
  */
  int main(void)
  {
   puts("\"Programming is like building a multilingual puzzle");
   return(0);
  }
-
-/**
- * Return: 0
- */
