16af2e3738dba3bc04dfa0eac4b9d3df26a1d7f8
Kalson <Kalson.Kalu@yahoo.com>
2016-10-13 02:49:01 +0000

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-	
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
