cfdc93dd142c169515f1beb29c7555c081212c3a
Kalson Kalu <Kalson.Kalu@yahoo.com>
2016-10-12 17:44:10 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main - print the string in the put function
- * @parameterx: Description of parameter x
- 
- * Description: We're using puts the print out a sring instead of printf)?
-(* section header: Section description)*
+ * 
+ * Description: using the main function
+*
  * Return: 0 because we're just printing a string
  */
  int main(void)
