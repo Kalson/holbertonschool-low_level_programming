9a70627f8df43183591ece939dc1933a663ee589
Kalson Kalu <Kalson.Kalu@yahoo.com>
2016-10-12 17:11:02 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
- int main(void){
- puts("\"Programming is like building a multilingual puzzle");
+ int main(void)
+ {
+  puts("\"Programming is like building a multilingual puzzle");
   return(0);
-}
+ }
