8bd0ab4e37333b5f799d18436f116241d0515150
Kalson <Kalson.Kalu@yahoo.com>
2016-10-14 21:17:24 +0000

    New

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,17 @@
  */
 int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-	putchar(c);
-if (c != '9')
-{
-	putchar(',');
-	putchar(' ');
-}
-}
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
-return (0);
+	return (0);
 }
