96f496859c2217c819174f58b499f4222bf15138
Kalson <Kalson.Kalu@yahoo.com>
2016-10-19 11:50:07 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; c++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
