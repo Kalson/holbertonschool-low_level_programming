d47a76f82a3231ccf7f8b5fcbc7dd52b57005d1f
Kalson <Kalson.Kalu@yahoo.com>
2016-10-19 04:15:26 +0000

    new

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+int main(void)
+{
+  char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+  unsigned int c;
+
+  for (c = 0; c < sizeof(holberton); c++)
+ {
+   _putchar(holberton[c]);
+ }
+_putchar('\n');
+return (0);
+}
