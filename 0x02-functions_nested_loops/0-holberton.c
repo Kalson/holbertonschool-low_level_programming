aec604f8634caf15c8002e44d03f3018acff843a
Kalson <Kalson.Kalu@yahoo.com>
2016-10-19 11:15:16 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,19 @@
 #include "holberton.h"
 
+/**
+ * main - function to print
+ *
+ * Return: 0
+ */
 int main(void)
 {
-  char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-  unsigned int c;
+	char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	unsigned int c;
 
-  for (c = 0; c < sizeof(holberton); c++)
- {
-   _putchar(holberton[c]);
- }
+	for (c = 0; c < sizeof(holberton); c++)
+	{
+		_putchar(holberton[c]);
+	}
 _putchar('\n');
 return (0);
 }
