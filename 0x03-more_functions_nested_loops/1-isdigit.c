a57611f3aa1f9eb4c93b54e48f17771a8bef7e5c
Kalson <Kalson.Kalu@yahoo.com>
2016-10-20 19:02:41 +0000

    isdigit

--- 0x03-more_functions_nested_loops/1-isdigit.c
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
-
+/**
+ * _isdigit - function that checks for uppercase character.
+ * @c: the int for the paramaters of my function
+ * Return: 0
+ */
 int _isdigit(int c)
 {
-  if (c >= 48 && c <= 57)
-    {
-      return (1);
-    }
-else
-  return (0);
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	else
+	return (0);
 }
