ff84b2bde2c073d4bd96a08f321e23a4461ae86a
Kalson <Kalson.Kalu@yahoo.com>
2016-10-20 22:16:56 +0000

    The numbers speak for themselves

--- 0x03-more_functions_nested_loops/3-print_numbers.c
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,16 @@
 #include "holberton.h"
-
+/**
+ * print_numbers - function that checks for uppercase character.
+ * Return: 0
+ */
 void print_numbers(void)
 {
-  char c = '0';
-  while (c <= '9' )
-    {
-      _putchar(c);
-	c++;
-    }
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
 _putchar('\n');
 }
