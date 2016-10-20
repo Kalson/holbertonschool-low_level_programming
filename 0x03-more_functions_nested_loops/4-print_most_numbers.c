6e5a1f500b3c4454932b6d953489dd870d461763
Kalson <Kalson.Kalu@yahoo.com>
2016-10-20 17:28:07 +0000

    I believe in numbers and signs

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	char c = '0';
+	
+	while (c <= '9')
+	{
+	  if (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
+		c++;
+	}
+_putchar('\n');
+}
