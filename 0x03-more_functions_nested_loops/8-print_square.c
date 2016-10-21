1c40eccc2fe139f1de67753669c7c10b97ebc234
Kalson <Kalson.Kalu@yahoo.com>
2016-10-21 00:49:53 +0000

    You are so much sunshine in every square inch

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_square - function that checks for uppercase character.
+ * @size: the int for the paramaters of my function
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int a = 0;
+	int b = 0;
+
+	if (size > 0)
+	{
+		while (a < size)
+		{
+			while (b < size)
+			{
+				_putchar('#');
+				b++;
+			}
+		b = 0;
+		a++;
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
