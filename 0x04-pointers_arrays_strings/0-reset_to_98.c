ba95250d422c844f61eb0aec1418ff97f0a23fb6
Kalson <Kalson.Kalu@yahoo.com>
2016-10-24 20:51:24 +0000

    98 Battery st.

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: declaration of n and paramters for the function reset_to_98
+ * Return: Always 0.
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
