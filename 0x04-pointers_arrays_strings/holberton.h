00d97417b5e41d430b7a30091f316af61bd7e452
Kalson <Kalson.Kalu@yahoo.com>
2016-10-25 19:25:48 +0000

    added holberton file

---
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+
+#endif
