ce97a1ac157cc79888748243bece9dbe8258532b
Kalson <Kalson.Kalu@yahoo.com>
2016-10-25 20:09:54 +0000

    I can only go one way. I've not got a reverse gear

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_rev - function that prints a string, followed by a new line
+ * @s: declaration of s and paramters for the function
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+        int c;
+
+	for (c = '\0'; s[c] != 0; c++)
+	{	
+	}
+	for (c = c; s[c] >= 0; c--)
+		_putchar(s[c]);
+	_putchar('\n');
+	
+}
