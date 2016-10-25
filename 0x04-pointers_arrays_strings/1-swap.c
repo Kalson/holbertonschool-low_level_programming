ed788e65af71127846629958baad553382aca2d1
Kalson <Kalson.Kalu@yahoo.com>
2016-10-25 19:36:30 +0000

    Don't swap horses in crossing a stream

--- 0x04-pointers_arrays_strings/1-swap.c
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,14 @@
 #include "holberton.h"
-
+/**
+ * swap_int - check the code for Holberton School students.
+ * @a: declaration of a and paramters for the function swap_int
+ * @b: declaration of b and paramters for the function swap_int
+ * Return: Always 0.
+ */
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
