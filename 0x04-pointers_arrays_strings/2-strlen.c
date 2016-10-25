e37f424d5ddbd38c0375cc3be1b7df3f876ada35
Kalson <Kalson.Kalu@yahoo.com>
2016-10-25 19:39:35 +0000

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
+}
