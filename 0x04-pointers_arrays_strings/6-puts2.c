74f2e167b75afb38552bb4aad50f19cc14303b20
Kalson <Kalson.Kalu@yahoo.com>
2016-10-26 02:50:41 +0000

    Half the lies they tell about me aren't true

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * puts2 - func that prints 1 char/2 of a string, followed by a new line.
+ * @str: declaration of str and paramters for the function puts2
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int c;
+	char l;
+
+	for (c = 0; str[c] != 0; c++)
+	{
+		if (c % 2 == 0)
+		{
+			l = str[c];
+			_putchar(l);
+		}
+	}
+	_putchar('\n');
+}
