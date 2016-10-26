704c33c6836b51bf2ca27c748a3fa8d08c18caeb
Kalson <Kalson.Kalu@yahoo.com>
2016-10-26 03:49:59 +0000

    update

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -6,8 +6,7 @@
  */
 void puts_half(char *str)
 {
-	int c;
-	int a;
+	int c, a;
 	char l;
 
 	for (c = 0; str[c] != 0; c++)
@@ -16,14 +15,6 @@ void puts_half(char *str)
 	if (c % 2 == 0)
 	{
 		for ((a = c / 2); str[a] != 0; a++)
-		{
-			l = str[a];
-			_putchar(l);
-		}
-	}
-	else
-	{
-		for ((a = (c - 1) / 2); str[a] != 0; a++)
 		{
 			l = str[a];
 			_putchar(l);
