dec1f4f74e7df7677a545e654e79a28fa243a10c
Kalson <Kalson.Kalu@yahoo.com>
2016-11-02 21:44:43 +0000

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: points to char array or string
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	unsigned int index = 0;
+	if (s[index] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[index]);
+	_puts_recursion(s + 1);
+}
