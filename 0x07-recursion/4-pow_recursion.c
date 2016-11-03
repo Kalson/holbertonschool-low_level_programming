3ad7f41d77d88c588c427ab3174b175040215ed8
Kalson <Kalson.Kalu@yahoo.com>
2016-11-03 05:50:59 +0000

    Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <math.h>
+/**
+ * _pow_recursion - func that returns the value of x raised to the power of y
+ * @x: paramater for the number
+ * @y: number to be raised to
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return ((int)pow(x, y));
+}
