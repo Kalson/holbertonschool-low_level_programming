172270f2484c354e004529c10b8abacd6ed1d90f
Kalson <Kalson.Kalu@yahoo.com>
2016-11-03 06:40:49 +0000

    updated header file

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -3,6 +3,13 @@
 
 int _putchar(char c);
 void _puts_recursion(char *s);
-
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int helper(int n, int i);
+int is_prime_number(int n);
+int helper_prime(int n, int i, int limit);
 
 #endif
