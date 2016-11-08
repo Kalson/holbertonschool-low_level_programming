b32b3c15fadf37496a8c9a7dd8dd1bc71d7cddde
Kalson <Kalson.Kalu@yahoo.com>
2016-11-08 02:59:02 +0000

    Neither irony or sarcasm is argument

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -10,17 +10,14 @@ int main(int argc, char *argv[])
 {
 	int num_1, num_2, mul;
 
-	if (argc > 1)
+	if (argc != 3)
+		printf("Error\n");
+	else
 	{
 		num_1 = atoi(argv[1]);
 		num_2 = atoi(argv[2]);
 		mul = num_1 * num_2;
-
 		printf("%d\n", mul);
-	}
-	else
-	{
-		printf("Error\n");
 	}
 	return (0);
 }
