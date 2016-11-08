09ba914c9e74331c37ce88aa32bb8682e0da1e90
Kalson <Kalson.Kalu@yahoo.com>
2016-11-08 01:25:37 +0000

    It ain't what they call you, it's what you answer to

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
-int main (int argc, char *argv[])
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
