7bd4cb427a2b93c51891ab682d0c44063dde1012
Emily Hendricks <Emily@Emilys-MacBook-Pro.local>
2016-11-14 23:12:19 -0800

    Django

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H_FILE
+#define DOG_H_FILE
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ */
+typedef struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
