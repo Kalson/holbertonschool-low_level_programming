0fe589adfa2003ccf3f8e79a22eb8b2f1aa25d80
Kalson <Kalson.Kalu@yahoo.com>
2016-11-17 22:14:03 +0000

    To hell with circumstances; I create opportunities

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return i;
+		}
+	}
+	return (-1);
+}
--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef HOLBERTON_H_FILE
 #define HOLBERTON_H_FILE
 #include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
